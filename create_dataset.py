5ceaa637c38ca84778cd8defc9cd878b3591ae3b
Mads Ehrhorn <mae@venzo.com>
2020-10-04 07:39:01 +0000

    DS file, rename main.py, add Azure pips, bash def

---
+++ create_dataset.py
@@ -0,0 +1,7 @@
+from azure.common.client_factory import get_client_from_cli_profile
+from azure.mgmt.resource import SubscriptionClient
+
+subscription_client = get_client_from_cli_profile(SubscriptionClient)
+
+subscription = next(subscription_client.subscriptions.list())
+print(subscription.subscription_id)
