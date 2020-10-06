39305431cee26102a1c7864d9e910226192a0be3
Mads Ehrhorn <mae@venzo.com>
2020-10-06 06:25:38 +0000

    Different updates.

--- create_dataset.py
+++ create_dataset.py
@@ -1,7 +1,10 @@
 from azure.common.client_factory import get_client_from_cli_profile
+from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import SubscriptionClient
 
 subscription_client = get_client_from_cli_profile(SubscriptionClient)
 
 subscription = next(subscription_client.subscriptions.list())
-print(subscription.subscription_id)
+print(dir(subscription))
+
+credential = DefaultAzureCredential()
