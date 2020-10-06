a188dc25e4c2c7a523bda19ed502f010fa4a1b9a
Mads Ehrhorn <mae@venzo.com>
2020-10-06 08:24:13 +0000

    Fixed create_dataset.py; ignoring .vscode

--- create_dataset.py
+++ create_dataset.py
@@ -1,11 +1,22 @@
 from azure.common.client_factory import get_client_from_cli_profile
 from azure.identity import DefaultAzureCredential
 from azure.mgmt.resource import SubscriptionClient
+from azure.mgmt.resource import ResourceManagementClient
+from azure.mgmt.storage import StorageManagementClient
+
 
 subscription_client = get_client_from_cli_profile(SubscriptionClient)
 
 subscription = next(subscription_client.subscriptions.list())
-print(dir(subscription))
+credentials = DefaultAzureCredential(
+    exclude_environment_credential=True,
+    exclude_managed_identity_credential=True,
+    exclude_visual_studio_code_credential=True,
+    exclude_shared_token_cache_credential=True,
+)
 
-credential = DefaultAzureCredential()
-print(credential)
+resource_client = ResourceManagementClient(credentials, subscription.subscription_id)
+storage_client = StorageManagementClient(credentials, subscription.subscription_id)
+
+for item in storage_client.storage_accounts.list():
+    print(item.name)
